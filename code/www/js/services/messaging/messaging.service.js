11c7eca1d0a7a3d8f72ea3d12d83e108c9f0312e
Dave Mee <davemee@gmail.com>
2019-11-19 12:01:11 +0000

    handles teardown message case

--- code/www/js/services/messaging/messaging.service.js
+++ code/www/js/services/messaging/messaging.service.js
@@ -22,6 +22,7 @@
   ) {
 
     var service = {
+      callbacks: { },
       deviceIdKey: "push_device_id"
     };
 
@@ -51,6 +52,10 @@
       return deferred.promise;
     };
 
+    service.subscribe = pushService.subscribe;
+
+    service.unsubscribe = pushService.unsubscribe;
+
     service.getDeviceId = function getDeviceId() {
       return(
         userSettingsService.asyncGet( service.deviceIdKey )
@@ -72,45 +77,73 @@
       });
     };
 
-    service.makeUUID = function makeUUID() {
-      // thanks https://stackoverflow.com/a/2117523
-      return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(
-        /[018]/g, c =>
-          (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).
-          toString(16)
-      );
+    service.handleInboundDisconnection = function handleInboundDisconnection( message ) {
+      // the subscriber PushNotificationService recognises the connection_teardown.
+      // the subscriber PushNotificationService calls unsubscribe() on the PushProviderPlugin, using the PushMessage senderId.
+      // The connection is now no longer accessible, and the 2 devices cannot communicate, without re-establishing the connection.
+      // PushNotificationService passes the message to the inboundMessageCallback (out to the rest of the app)
+
+    }
+
+    service.addInboundMessageCallback = function addInboundMessageCallback(id, fnCallback) {
+      service.callbacks[ id ] = fnCallback;
+    };
+
+    service.removeInboundMessageCallback = function removeInboundMessageCallback(id) {
+      if(service.callbacks.hasOwnProperty( id ) === true ) {
+        delete service.callbacks.id;
+      }
     };
 
     service.inboundHandler = function inboundHandler( message ) {
       var eventName = "message."+message.payload.hasOwnProperty("message_type")?
-          message.payload.message_type:service.message_type.NONE;
+          message.payload.message_type:
+          service.message_type.NONE;
       // message_type is one of NONE, CONNECTION_REQUEST, CONNECTION_RESPONSE, CONNECTION_TEARDOWN, SYSTEM_MESSAGE, MESSAGE
 
-      pubsubService.asyncPublish( eventName, message );
+      // using the pubsubService:
+      // pubsubService.asyncPublish( eventName, message );
 
-      //$rootScope.$broadcast(eventName, message.payload );
+      // using angularjs broadcast system:
+      //$rootScope.$broadcast(eventName, message );
       //  usage:
       //      $scope.$on('message.CONNECTION_REQUEST', function(e, data){
       //        // handle message
       //     });
+
+      // handle teardown - https://trello.com/c/G1eEO92o/20-pushnotificationservice
+      if(message.payload.hasOwnProperty("message_type")) {
+        if( message.payload.message_type === service.message_type.CONNECTION_TEARDOWN ) {
+          pushService.unsubscribe( message.sender_id );
+        }
+      }
+
+      for(var key in service.callbacks) {
+        service.callbacks[key]( message );
+      }
+
     };
 
-    service.send = function send(p) {
+    service.sendMessage = function sendMessage(p) {
       if(p.payload.hasOwnProperty("dateStamp")===false){
         p.payload.dateStamp = new Date().getTime();
       }
       if(p.payload.hasOwnProperty("uuid")===false) {
-        p.payload.uuid = service.makeUUID();
+        function makeUUID() { // thanks https://stackoverflow.com/a/2117523
+          return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(
+            /[018]/g, function(c){
+              (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).
+                toString(16);
+            }
+          );
+        };
+        p.payload.uuid = makeUUID();
       }
       if(p.payload.hasOwnProperty("message_type")===false) {
         p.payload.message_type = service.message_type.MESSAGE;
       }
       pushService.sendPayload(p);
     };
-
-    service.subscribe = pushService.subscribe;
-
-    service.unsubscribe = pushService.unsubscribe;
 
     return service;
   }
