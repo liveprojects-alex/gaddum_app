c64a5510790621a543b1fdc103fe018a1bfe9dd6
Coops <coopsatwork@googlemail.com>
2019-11-28 16:17:31 +0000

    Corrected startup behaviour - now remembers music provider on startup

--- code/www/sql/get_selected_music_provider.sql
+++ code/www/sql/get_selected_music_provider.sql
@@ -1,6 +1,6 @@
 with get_provider_setting as
 (
-select value from settings where id = ?1
+select value from settings where id = 'music_provider_id'
 ),
 get_provider_detail as (
 select t1.* from music_providers t1 inner join get_provider_setting t2 on t1.id = t2.value 
