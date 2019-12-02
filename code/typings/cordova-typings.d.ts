b3ed5230f41cde31fce5ab45ae944a73068d89a0
andrzejbuca <andrzejbuca@users.noreply.github.com>
2019-12-02 15:44:10 +0000

    :)

--- code/typings/cordova-typings.d.ts
+++ code/typings/cordova-typings.d.ts
@@ -9,3 +9,9 @@
 /// <reference path="..\.vscode\typings\ionic\ionic.d.ts"/>
 /// <reference path="..\.vscode\typings\angularjs\angular.d.ts"/>
 /// <reference path="..\.vscode\typings\jquery\jquery.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Device.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Splashscreen.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\StatusBar.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\FileSystem.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\NetworkInformation.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\InAppBrowser.d.ts"/>
